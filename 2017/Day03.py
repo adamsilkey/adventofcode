d23bb8d9f59ad03267e0857e582cbfaca7d083a6
Adam Silkey <adam.silkey@gmail.com>
2017-12-03 03:15:55 -0500

    Clean up Day 2, Part 2 code

--- 2017/Day03.py
+++ 2017/Day03.py
@@ -1,95 +1,121 @@
 import sys
+try:
+    import numpy as np
+except ImportError:
+    np = None
+
+# --- Constants ----------------------------------------------------------------
 
 puzzle_input = 277678
 
-# a = [1,1,2,4,5,10,11,23,25,26,54,57,59,122,133,142,147,304,330,351,362,747,806,880,931]
-
-# print(a[0:1])
-# print(a[1:9])   # 8 elements
-# print(a[9:25])  # 16 elements
-
-
-sample_array = [[0, 0,   0,  0, 0],
-                [0, 5,   4,  2, 0],
-                [0, 10,  1,  1, 0],
-                [0, 11, 23,  0, 0],
-                [0, 0,   0,  0, 0]]
-
-
-N, S, E, W = (0, -1), (0, 1), (1, 0), (-1, 0) #directions
+#directions
+N = (0, -1) 
+W = (-1, 0)
+S = (0, 1)
+E = (1, 0)
 turn_left = {N: W, W: S, S: E, E: N} # old: new direction
 
-def add_surround(array, x, y):
-    # doesn't handle out of bounds... even though it used to, I DELETED THE CODE
-    sum_ = 0
+# --- Functions ----------------------------------------------------------------
 
-    #North 
-    sum_ += array[y][x-1]
-    #NorthEast
-    sum_ += array[y+1][x-1]
-    #East
-    sum_ += array[y+1][x]
-    #SouthEast
-    sum_ += array[y+1][x+1]
-    #South
-    sum_ += array[y][x+1]
-    #SouthWest
-    sum_ += array[y-1][x+1]
-    #West
-    sum_ += array[y-1][x]
-    #NorthWest
-    sum_ += array[y-1][x-1]
+def add_surround(array, x, y):
+    '''Adds the surrounding nums per the problem'''
+    sum_ = 0
+    north = None if y-1 < 0 else y-1
+    west = None if x-1 < 0 else x-1
+    south = None if y+1==len(array) else y+1
+    east = None if x+1==len(array[0]) else x+1
+
+    try:
+        sum_ += array[north][x]
+    except TypeError:
+        pass
+    try:
+        sum_ += array[north][east]
+    except TypeError:
+        pass
+    try:
+        sum_ += array[y][east]
+    except TypeError:
+        pass
+    try:
+        sum_ += array[south][east]
+    except TypeError:
+        pass
+    try:
+        sum_ += array[south][x]
+    except TypeError:
+        pass
+    try:
+        sum_ += array[south][west]
+    except TypeError:
+        pass
+    try:
+        sum_ += array[y][west]
+    except TypeError:
+        pass
+    try:
+        sum_ += array[north][west]
+    except TypeError:
+        pass
     
     return sum_
 
-assert(add_surround(sample_array, 3, 3) == 25)
+def pad_array(array, pad=0):
+    '''Pads a rectangular array with the pad character'''
+    for row in array:
+        row.insert(0, pad)
+        row.append(pad)
+    length = len(array[0])
+    array.insert(0, [pad] * length)
+    array.append([pad] * length)
+
+def spiral_move(array, x, y, dx, dy):
+    '''Spiral moves in direction or rotates if it can. Pads zeroes as necessary'''
+    x += dx
+    y += dy
+    if x < 0 or y < 0 or x == len(array[0]) or y == len(array):
+        pad_array(array)
+        x += 1
+        y += 1
+    new_dx, new_dy = turn_left[dx, dy]
+    if array[y+new_dy][x+new_dx] == 0:
+        dx, dy = new_dx, new_dy
+    return x, y, dx, dy
 
 def go(limit, initial_direction):
-    array = [[0, 0, 0],
-             [0, 1, 0],
-             [0, 0, 0]]
-    x, y = 1, 1
+    array = [[1]]
+    x, y = 0, 0
     dx, dy = initial_direction
     num = array[y][x]
 
     while num < limit:
-        try:
-            array[y][x+2]   #See if we need to add another block of padding zeros
-        except IndexError:
-            # With an index error, we pad our matrix with 0s
-            for row in array:
-                row.insert(0,0)
-                row.append(0)
-            length = len(array[0])
-            array.insert(0, [0] * length)
-            array.append([0] * length)
-            x += 1
-            y += 1
-
-        # see if we need to rotate
-        new_dx, new_dy = turn_left[dx,dy]
-        if array[y+new_dy][x+new_dx] == 0:
-            dx, dy = new_dx, new_dy
-            print('you need to rotate')
-
-        x += dx
-        y += dy
-        print(x, y)
-        print(array[y][x])
-        print(add_surround(array, x, y))
+        x, y, dx, dy = spiral_move(array, x, y, dx, dy)
         array[y][x] = add_surround(array, x, y)
-
         num = array[y][x]
 
-    #while block ended
-    for row in array:
-        print(row)
+    if np:
+        print(np.matrix(array)) 
+    else:
+        for row in array:
+            print(row)
+    print()
+    print(f"Last number found was: {array[y][x]}")
 
-    print(x,y)
-    print(array[y][x])
+# --- Tests --------------------------------------------------------------------
+def test_add_surround():
+    sample_array = [[5,   4,  2], [10,  1,  1], [11, 23,  0]]
+    assert(add_surround(sample_array, 2, 2) == 25)
 
+def test_pad_array():
+    pad_array_test = [[1]]
+    pad_array(pad_array_test)
+    assert(pad_array_test == [[0,0,0],[0,1,0],[0,0,0]])
 
-go(puzzle_input+1, S)
-
-#go(2, E)
+# --- Main ---------------------------------------------------------------------
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Solve Advent of Code 2017 Day 3, Part 2')
+    parser.add_argument('limit', metavar='N', type=int, nargs='?', default=puzzle_input, help='Puzzle input')
+    args = parser.parse_args()
+    go(args.limit, E)
 
