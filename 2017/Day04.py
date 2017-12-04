009575c75e60fa68b8ce2250ef55e823acb7213e
Adam Silkey <adam.silkey@gmail.com>
2017-12-04 00:46:27 -0500

    Solve Day 4, Part 2

--- 2017/Day04.py
+++ 2017/Day04.py
@@ -16,7 +16,23 @@ def part_one():
         print(total)
 
 def part_two():
-    pass
+    print('part two')
+    with open('input.txt') as f:
+        total = 0
+        for line in f:
+            list_ = line.split()
+            set_ = list(set(line.split()))
+            if sorted(set_) == sorted(list_):
+                new_list = []
+                for item in list_:
+                    new_list.append(''.join(sorted(item)))
+                if [k for k,v in Counter(new_list).items() if v>1]:
+                    print('found dup')
+                else:
+                    print('found no dup')
+                    total += 1
+
+        print(total)
 
 if __name__ == '__main__':
     part_one()
