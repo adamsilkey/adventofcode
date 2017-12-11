a358d92bb1978474257c8e602c7270811a6b224d
Adam Silkey <adam.silkey@gmail.com>
2017-12-11 01:36:39 -0600

    Refactor Day 11 solution.
    Refactored Day 11 solution to be more pythonic. Added properties
    that enable us to access timmydistance and max.
    
    Swapped out tuples for named tuples instead.
    
    Changed the internal dict to instead map to the number of times
    Timmy has been to each location.
    
    Moved dirs inside the class to enable better subclassing.

--- 2017/Day11.py
+++ 2017/Day11.py
@@ -1,69 +1,58 @@
 import sys
-from collections import namedtuple
+
+from collections import defaultdict, namedtuple
 
 def parse_input(inp):
     with open(inp) as f:
         return f.readline().strip()
 
-
-
-
-class Hex:
-    def __init__(self, x, y, z):
-        self.pos = (x, y, z)
-        self.x = x
-        self.y = y
-        self.z = z
-        self.nw = (x - 1, y + 1, z + 0)
-        self.n = (x - 0, y + 1, z - 1)
-        self.ne = (x + 1, y + 0, z - 1)
-        self.sw = (x - 1, y + 0, z + 1)
-        self.s = (x - 0, y - 1, z + 1)
-        self.se = (x + 1, y - 1, z + 0)
-
-hex_dirs = {'nw': (-1,  1,  0),
-            'n':  ( 0,  1, -1),
-            'ne': ( 1,  0, -1),
-            'sw': (-1,  0,  1),
-            's':  ( 0, -1,  1),
-            'se': ( 1, -1,  0)}
-
+Hex = namedtuple('Hex', ['x', 'y', 'z'])
 
 class HexGrid:
     def __init__(self, instructions, x = 0, y = 0, z = 0):
         self.instructions = [instruction for instruction in instructions.split(',')]
-        self.grid = {(x,y,z): Hex(x,y,z)}
-        self.pos = (x,y,z)
-        self.max_x = x
-        self.max_y = y
-        self.max_z = z
+        self.grid = defaultdict(int)
+        self.grid[Hex(x,y,z)] += 1
+        self.pos = Hex(x,y,z)
+        self._max = {'x': self.pos.x, 
+                     'y': self.pos.y,
+                     'z': self.pos.z}
+
+    dirs = {'nw': Hex(-1,  1,  0),
+            'n':  Hex( 0,  1, -1),
+            'ne': Hex( 1,  0, -1),
+            'sw': Hex(-1,  0,  1),
+            's':  Hex( 0, -1,  1),
+            'se': Hex( 1, -1,  0)}
 
     def generate_grid(self):
         for ins in self.instructions:
-            new_pos = (self.pos[0] + hex_dirs[ins][0],
-                       self.pos[1] + hex_dirs[ins][1],
-                       self.pos[2] + hex_dirs[ins][2])
-            print(new_pos)
-            if new_pos not in self.grid:
-                self.grid[new_pos] = Hex(new_pos[0], new_pos[1], new_pos[2])
+            new_pos = Hex(self.pos.x + self.__class__.dirs[ins].x,
+                          self.pos.y + self.__class__.dirs[ins].y,
+                          self.pos.z + self.__class__.dirs[ins].z)
+            self.grid[new_pos] += 1
             self.pos = new_pos
-            if abs(self.pos[0]) > self.max_x:
-                self.max_x = abs(self.pos[0])
-            if abs(self.pos[0]) > self.max_y:
-                self.max_y = abs(self.pos[1])
-            if abs(self.pos[0]) > self.max_z:
-                self.max_z = abs(self.pos[2])
+            if abs(self.pos.x) > self._max['x']:
+                self._max['x'] = abs(self.pos.x)
+            if abs(self.pos.y) > self._max['y']:
+                self._max['y'] = abs(self.pos.y)
+            if abs(self.pos.z) > self._max['z']:
+                self._max['z'] = abs(self.pos.z)
+            print(self.pos, self.grid[self.pos])
 
-    def p(self):
-        for instruction in self.instructions:
-            print(instruction)
-        for key, val in self.grid:
-            print(key, val)
+    @property
+    def timmydistance(self):
+        return max(abs(self.pos.x), abs(self.pos.y), abs(self.pos.z))
+
+    @property
+    def max(self):
+        return max(self._max.values())
 
 def solve(inp):
     h = HexGrid(inp, 0, 0, 0)
     h.generate_grid()
-    print(h.max_x, h.max_y, h.max_z)
+    print(h.timmydistance)
+    print(h.max)
     print()
     return inp
 
@@ -72,8 +61,4 @@ solve('ne,ne,sw,sw')        #0 steps - back to where you started
 solve('ne,ne,s,s')          #2 steps - se, se
 solve('se,sw,se,sw,sw')     #3 steps - s, s, sw
 
-
 solve(parse_input('input.txt'))
-
-
-
