017494fdaccaebfe676919942f915ef50fe65be4
Adam Silkey <adam.silkey@gmail.com>
2017-12-12 00:28:44 -0500

    Solve Day 12, Part 2

--- 2017/Day12.py
+++ 2017/Day12.py
@@ -15,15 +15,46 @@ def parse_input(filename):
             dic[int(idx)] = l
         return dic
 
+'''
 def pipewalk(check_num, pipe_dict, pipe_list):
     for i in pipe_dict[check_num]:
         if i not in pipe_list:
             pipe_list.append(i)
             pipewalk(i, pipe_dict, pipe_list)
+'''
+
+
+def pipewalk(check_num, pipe_dict, pipe_list = None):
+    if pipe_list is None:
+        pipe_list = []
+    for i in pipe_dict[check_num]:
+        if i not in pipe_list:
+            pipe_list.append(i)
+            pipewalk(i, pipe_dict, pipe_list)
+
+    return pipe_list
+
 
 def solve(inp):
 
-    pipewalk(0, inp, pipelist)
+    pipegroups = []
+
+    while inp:
+        pipe = inp.popitem()
+        inp[pipe[0]] = pipe[1]
+        group = pipewalk(pipe[0], inp)
+        for key in group:
+            del inp[key]
+        pipegroups.append(group)
+
+
+    print(pipegroups)
+    print(len(pipegroups))
+
+    #pipewalk(0, inp, pipelist)
+
+
+
     print(pipelist)
     print(len(pipelist))
     return inp
@@ -31,3 +62,4 @@ def solve(inp):
 
 #sys.exit()
 solve(parse_input('input.txt'))
+
