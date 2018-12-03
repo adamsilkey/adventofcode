e0d6f29129560aecff89eaaa6940df8f4458050c
Adam Silkey <adam.silkey@gmail.com>
2018-12-03 17:46:28 -0600

    Solve 2018 Day 1 Part 2

--- 2018/Day01.py
+++ 2018/Day01.py
@@ -8,4 +8,22 @@ with open("Day01Input.txt") as f:
 
 print(f"Frequency is {frequency}")
 
+print("Part 2")
+frequency = 0
+seen_frequencies = set()
+repeated = False
+
+with open("Day01Input.txt") as f:
+    frequency_input = [int(line.strip()) for line in f]
+
+while repeated is False:
+    for i in frequency_input:
+        frequency += i
+        if frequency not in seen_frequencies:
+            seen_frequencies.add(frequency)
+        else:
+            print(f"Repeated frequency: {frequency}")
+            repeated = True
+            break
+
 
