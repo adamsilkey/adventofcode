448024de03910e8a8e67d55b610864d3f6371551
Adam Silkey <adam.silkey@gmail.com>
2020-12-03 00:27:40 -0600

    Final Day 3 version
    - Implement modulo
    - Remove IndexError

--- 2020/day03.py
+++ 2020/day03.py
@@ -1,36 +1,27 @@
 #! /usr/bin/env python3
 
-import sys
-
 print("day 3")
 
 with open("input/2020-03.in") as f:
-    day3 = [line.strip() for line in f]
+    trees = [line.strip() for line in f]
 
-trees = [line * 90 for line in day3]
-
+tree_len = len(trees[0])
 tree = "#"
-
 a = b = c = d = e = 0
 
-try:
-    for idx, line in enumerate(trees):
-        if trees[idx][idx*1] == tree:
-            a += 1
-        if trees[idx][idx*3] == tree:
-            b += 1
-        if trees[idx][idx*5] == tree:
-            c += 1
-        if trees[idx][idx*7] == tree:
-            d += 1
-        if trees[idx][idx//2] == tree:
-            if idx % 2 == 0:
-                e += 1
-
-except IndexError:
-    print("treeline not big enough")
-    exit()
+for idx, line in enumerate(trees):
+    if trees[idx][(idx * 1) % tree_len] == tree:
+        a += 1
+    if trees[idx][(idx * 3) % tree_len] == tree:
+        b += 1
+    if trees[idx][(idx * 5) % tree_len] == tree:
+        c += 1
+    if trees[idx][(idx * 7) % tree_len] == tree:
+        d += 1
+    if trees[idx][(idx // 2) % tree_len] == tree:
+        if idx % 2 == 0:
+            e += 1
 
 print(f"Part One: {b}")
-print(f"Part Two: {(a,  b, c, d, e)}")
+print(f"Part Two: {(a, b, c, d, e)}")
 print(f"Part Two: {a * b * c * d * e}")
