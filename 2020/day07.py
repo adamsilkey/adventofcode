e4744d6fec23f0363e654660251ec8605336783e
Adam Silkey <adam.silkey@gmail.com>
2020-12-07 04:54:22 -0600

    Cleanup 2020 - Day 07

--- 2020/day07.py
+++ 2020/day07.py
@@ -21,6 +21,8 @@ faded blue bags contain no other bags.
 dotted black bags contain no other bags.
 """.strip().split("\n")
 
+test_data = [line.split() for line in test_data]
+
 part_two_test_data = """
 shiny gold bags contain 2 dark red bags.
 dark red bags contain 2 dark orange bags.
@@ -31,12 +33,14 @@ dark blue bags contain 2 dark violet bags.
 dark violet bags contain no other bags.
 """.strip().split("\n")
 
-test_data = [line.split() for line in test_data]
 part_two_test_data = [line.split() for line in part_two_test_data]
 
 
+DATASET = day7
+
+
 def generate_bag_rules(raw_data):
-    bag_types = {}
+    bag_rules = {}
     for rule in raw_data:
         bag = "".join(rule[0:2])
         contents = {}
@@ -48,141 +52,39 @@ def generate_bag_rules(raw_data):
                 contents[name] = qty
                 rule = rule[4:]
 
-        bag_types[bag] = contents
+        bag_rules[bag] = contents
 
-    return bag_types
-
-
-# bags = generate_bag_rules(test_data)
-
-# bags = generate_bag_rules(part_two_test_data)
-#
-bags = generate_bag_rules(day7)
+    return bag_rules
 
 
-def get_bag_contents(bag_list: dict, outer_bag: str, contents: dict, factor=1):
-    for bag in bag_list[outer_bag]:
-        qty = bag_list[outer_bag][bag]
+def get_bag_contents(bag_rules: dict, outer_bag: str, contents=None, factor=1):
+    if contents is None:
+        contents = {}
+
+    for bag in bag_rules[outer_bag]:
+        qty = bag_rules[outer_bag][bag]
         if bag not in contents:
             contents[bag] = 0
         contents[bag] += qty * factor
 
-        if bag_list[bag]:
-            get_bag_contents(bag_list, bag, contents, qty * factor)
-
-
-        # if bag_list[bag]:
-        #     for _ in range(bag_list[outer_bag][bag]):
-        #         get_bag_contents(bag_list, bag, contents)
+        if bag_rules[bag]:
+            get_bag_contents(bag_rules, bag, contents, qty * factor)
 
     return contents
 
 
-all_bags = []
+def solve(dataset):
+    bag_rules = generate_bag_rules(dataset)
+    bags = {bag: get_bag_contents(bag_rules, bag) for bag in bag_rules}
 
-for bag in bags:
-    bag_contents = {}
-    # print(bag, get_bag_contents(bags, bag, bag_contents))
-    all_bags.append(get_bag_contents(bags, bag, bag_contents))
+    shinygold = 0
+    for contents in bags.values():
+        if "shinygold" in contents:
+            shinygold += 1
 
-
-shinygold = 0
-for bag in all_bags:
-    if "shinygold" in bag:
-        shinygold += 1
-
-print(f"part one: {shinygold}")
-
-shiny_contents = get_bag_contents(bags, "shinygold", {})
-
-sum = 0
-for q in shiny_contents.values():
-    sum += q
-
-
-print(f"part two: {sum}")
-
-
-'''
-"""def part_one(bag: str, bag_list: dict, bag_target: str):
-    print(f"Checking {bag}")
-    print(f"Searching through {bag_list[bag]}")
-    for b in bag_list[bag]:
-        print(f"  - checking {b}")
-        if b == bag_target:
-            print("found our target!")
-            return True
-        elif not bag_list[b]:
-            print("found an empty bag")
-            continue
-        else:
-            return part_one(b, bag_list, bag_target)
-    print("End of the line")
-    print()
-    # return False
-"""
-
-
-def get_bag_contents(bag: str, bag_list: dict, contents=None):
-    if contents is None:
-        contents = set()
-    for sub_bag in bag_list[bag]:
-        contents.add(sub_bag)
-        if bag_list[sub_bag]:
-            get_bag_contents(sub_bag, bag_list, contents)
-
-    return contents
-
-
-def part_one(bag_list, target):
-    target_count = 0
-    for i, bag in enumerate(bag_list):
-        if target in get_bag_contents(bag, bag_list):
-            target_count += 1
-        print(f"Processed {i + 1} / {len(bag_list)} bags.")
-
-    return target_count
-
-
-
-
-def test():
-    test_data = """
-    dumb red bags contain 1 dotted black bag, 1 slime blue bag
-    slime blue bags contain 1 light red bag
-    light red bags contain 1 bright white bag, 2 muted yellow bags.
-    dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-    bright white bags contain 1 shiny gold bag.
-    muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-    shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-    dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-    vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-    faded blue bags contain no other bags.
-    dotted black bags contain no other bags.
-    """.strip().split("\n")
-
-    test_data = [line.split() for line in test_data]
-
-    bags = generate_bag_rules(test_data)
-    print(f"part_one {part_one(bags, 'shinygold')}")
-
-
-
-
-def main():
-    bags = generate_bag_rules(day7)
-
-    print(f"part_one {part_one(bags, 'shinygold')}")
+    print(f"part one: {shinygold}")
+    print(f"part two: {sum(bags['shinygold'].values())}")
 
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 2:
-        print("specifcy debug or prod")
-        sys.exit()
-    debug = sys.argv[1]
-    if debug == "debug":
-        test()
-    else:
-        main()
-        '''
+    solve(DATASET)
