47f1c78024fdb5b5f4615b2031136715801ce8ed
Adam Silkey <adam.silkey@gmail.com>
2020-12-08 02:11:21 -0600

    Solve 2020 - Day 08 - Part 2

--- 2020/day08.py
+++ 2020/day08.py
@@ -1,11 +1,86 @@
 #! /usr/bin/env python3
 
+from collections import namedtuple
+from typing import List
+
 print("day 8")
 
 with open("input/2020-08.in") as f:
     day8 = [line.strip() for line in f]
 
-part_two_test = """
+Instruction = namedtuple('Instruction', 'line ins value')
+
+
+def generate_instructions(data):
+    instructions = []
+    for idx, line in enumerate(data):
+        move, value = line.split()
+        if value.startswith("+"):
+            value = value[1:]
+        value = int(value)
+        instructions.append(Instruction(idx, move, value))
+
+    return instructions
+
+
+def runner(instructions: List[Instruction]):
+    acc = 0
+    line = 0
+    seen = {}
+    while True:
+        # Have we run out of instructions? If so, we break
+        if line >= len(instructions):
+            valid = True
+            break
+
+        # Get instruction based on our current line number
+        ins = instructions[line]
+
+        # Have we seen this instruction before? If so, we break
+        if ins not in seen:
+            seen[ins] = 1
+        else:
+            valid = False
+            break
+
+        # Handle instructions
+        if ins.ins == "nop":
+            line += 1
+        elif ins.ins == "jmp":
+            line += ins.value
+        elif ins.ins == "acc":
+            acc += ins.value
+            line += 1
+        else:
+            raise ValueError(f"Invalid instruction in set {ins.ins}")
+
+    Result = namedtuple("Result", "acc valid")
+
+    return Result(acc, valid)
+
+
+def part_two(instructions: List[Instruction]):
+    for ins in instructions:
+        tmp_instructions = instructions[:]
+        if ins.ins == "nop":
+            tmp = Instruction(ins.line, "jmp", ins.value)
+        elif ins.ins == "jmp":
+            tmp = Instruction(ins.line, "nop", ins.value)
+        elif ins.ins == "acc":
+            continue
+        tmp_instructions[ins.line] = tmp
+
+        acc, valid = runner(tmp_instructions)
+        if valid is True:
+            return acc
+
+
+day8 = generate_instructions(day8)
+print(f"part one: {runner(day8).acc}")
+print(f"part two: {part_two(day8)}")
+
+
+part_one_test = """
 nop +0
 acc +1
 jmp +4
@@ -15,42 +90,19 @@ acc -99
 acc +1
 jmp -4
 acc +6
-""".strip()
+""".strip().split("\n")
 
-test = part_two_test.strip().split("\n")
+part_two_test = """
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+nop -4
+acc +6
+""".strip().split("\n")
+test1 = generate_instructions(part_one_test)
+test2 = generate_instructions(part_two_test)
 
-instructions = []
-
-for idx, line in enumerate(day8):
-# for idx, line in enumerate(test):
-    move, value = line.split()
-    if value.startswith("+"):
-        value = value[1:]
-    value = int(value)
-    visits = 0
-    line_number = idx
-    check = False
-    instructions.append([move, value, visits])
-
-
-def part_one():
-    acc = 0
-    line = 0
-    while True:
-        ins = instructions[line]
-        ins[2] += 1
-        if ins[2] == 2:
-            break
-
-        if ins[0] == "nop":
-            line += 1
-        elif ins[0] == "acc":
-            line += 1
-            acc += ins[1]
-        else:       # jmp
-            line += ins[1]
-
-    print(f"part one: {acc}")
-
-
-part_one()
