60acd835f2643e7164752e55cdddb03e7c9c5897
Adam Silkey <adam.silkey@gmail.com>
2020-12-10 20:46:08 -0600

    Final 2020 - Day 10 Solution (with notes!)
    Our final 2020 Day 10 solution is maximum python readability as
    well as maximum algorithmic efficiency.
    
    We know that path lengths spike in possibility based on the chains
    with consecutive runs (gaps of 1 instead of gaps of 3). We also
    know (by analyzing the data) that the maximum "chain" size is 5.
    So we can that knowledge to mulitply out the possibilities.
    
    We can also calculate the run as we go.
    
    We take advantage of some special characteristics of enumerate()
    to simplify calling and logic.

--- 2020/day10.py
+++ 2020/day10.py
@@ -5,11 +5,7 @@ from itertools import combinations
 print("day 10")
 
 with open("input/2020-10.in") as f:
-    # day9 = [line.strip() for line in f]
-    day9 = f.readlines()
-for line in day9:
-    print(repr(line))
-
+    day10 = f.readlines()
 
 
 def parse_data(data):
@@ -20,137 +16,27 @@ def jolt_chain(adapters: list):
     adapters.append(0)
     adapters.sort()
     adapters.append(adapters[-1] + 3)
-    jolts = {1: 0, 2: 0, 3: 0}
 
-    chain = 1
-    total_paths = 1
+    # Part One
+    ones = 0
+    threes = 0
 
-    for idx, jolt in enumerate(adapters):
-        if idx + 1 == len(adapters):
-            break
-        jolt_difference = adapters[idx+1] - adapters[idx]
-        jolts[jolt_difference] += 1
+    # Part Two 
+    distinct_arrangements = 1
+    chain_to_combos = {1: 1, 2: 1, 3: 2, 4: 4, 5: 7}  # chain-length, combos
+    start = 0
 
-        if jolt_difference == 1:
-            chain += 1
-        else:
-            if chain == 3:
-                total_paths *= 2
-            elif chain == 4:
-                total_paths *= 4
-            elif chain == 5:
-                total_paths *= 7
-            chain = 1
+    # Iterate to the second to last item, start counting from second list element
+    for end, jolt_rating in enumerate(adapters[:-1], start=1):
+        if adapters[end] - jolt_rating == 1:
+            ones += 1
+        else:       # Must be 3
+            threes += 1
+            distinct_arrangements *= chain_to_combos[end - start]
+            start = i
 
-    print(f"day10 part_one: {jolts[1] * jolts[3]}")
-    print(f"day 10 part two : {total_paths}")
+    print(f"part one: {ones * threes}")
+    print(f"part two: {distinct_arrangements}")
 
 
-jolt_chain(parse_data(day9))
-import sys;sys.exit()
-
-# log(base2) of 8 = 
-
-# (0), 1,     4, 5, 6, 7,    10, 11, 12,      15, 16, 19, (22)
-# (0), 1,     4, 5, 6, 7,    10,     12,      15, 16, 19, (22)
-# (0), 1,     4, 5,    7,    10, 11, 12,      15, 16, 19, (22)
-# (0), 1,     4, 5,    7,    10,     12,      15, 16, 19, (22)
-# (0), 1,     4,    6, 7,    10, 11, 12,      15, 16, 19, (22)
-# (0), 1,     4,    6, 7,    10,     12,      15, 16, 19, (22)
-# (0), 1,     4,       7,    10, 11, 12,      15, 16, 19, (22)
-# (0), 1,     4,       7,    10,     12,      15, 16, 19, (22)
-
-
-# For consecutive numbers
-# 0 - 1               chain length of 2 -> 1 total routes  -- 2 ^ 0 (length)
-# 0 - 1 - 2 =         chain length of 3 -> 2 total routes  -- 2 ^ 1 (length of chain - 1)
-# 0 - 1 - 2 - 3 =     chain length of 4 -> 4 total routes  -- 2 ^ 2
-
-# 0 - 1 - 2 - 3 - 4 = chain length of 5 -> 7 total routes  -- 2 ^ 3 - 1 
-# 0 1 2 3 4 5         chain length of 6 -> 10 total routes?
-
-#   0   1   2   3   4
-#   0   1   2       4
-#   0   1       3   4
-#   0   1           4
-#   0       2   3   4
-#   0       2       4
-#   0           3   4
-#   0               4 (not a route)
-
-# chain length of 5
-# 0 1 2 3 4 5
-# 0 1 2 3   5
-# 0 1 2   4 5
-# 0 1   3 4 5
-# 0   2 3 4 5
-# 0   2   4 5
-# 0   2 3   5
-# 0   2     5
-# 0     3 4 5
-# 0     3   5
-# 0       4 5 (not a route)
-# 0         5 (not a route)
-
-
-
-def test():
-    test_data = """
-    16
-    10
-    15
-    5
-    1
-    11
-    7
-    19
-    6
-    12
-    4
-    """.strip().split("\n")
-
-    test_data = parse_data(test_data)
-    jolt_chain(test_data)
-
-test()
-
-# import sys;sys.exit()
-
-def test2():
-    test_data = """
-    28
-    33
-    18
-    42
-    31
-    14
-    46
-    20
-    48
-    47
-    24
-    23
-    49
-    45
-    19
-    38
-    39
-    11
-    1
-    32
-    25
-    35
-    8
-    17
-    7
-    9
-    4
-    2
-    34
-    10
-    3""".strip().split("\n")
-
-    test_data = parse_data(test_data)
-    jolt_chain(test_data)
-
-test2()
+jolt_chain(parse_data(day10))
