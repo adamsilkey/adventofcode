c4eb04b8605d5eb286715d51ad8dd356f75e022d
Adam Silkey <Adam.Silkey@gmail.com>
2021-12-05 02:24:15 -0600

    Cleanup 2021 - Day 05
    Refactored Day 05 to use two classes instead of a mix of random
    procedural code and classes.
    
    - Line class to handle parsing a string and creating a line
    - VentMap class to handle everything else
    
    Was also able to consolidate the class down into one that could
    handle diagonals and non-diagonals based on a flag.

--- 2021/day05.py
+++ 2021/day05.py
@@ -2,173 +2,85 @@
 
 TEST = False
 
-import itertools as it
 import re
-from collections import deque
-from dataclasses import dataclass
+
+class Line:
+    pattern = re.compile(r"(\d*),(\d*) -> (\d*),(\d*)")
+
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = int(x1)
+        self.y1 = int(y1)
+        self.x2 = int(x2)
+        self.y2 = int(y2)
+
+    @classmethod
+    def from_string(cls, s: str):
+        match = cls.pattern.match(s.strip())
+        x1 = match.group(1)
+        y1 = match.group(2)
+        x2 = match.group(3)
+        y2 = match.group(4)
+
+        return cls(x1, y1, x2, y2)
 
 
-def load_file(filename: str) -> str:
-    """Loads an AOC file, returns a string"""
-
-    with open(filename) as f:
-        return f.read().rstrip("\n")
-
-
-def load_lines(filename: str) -> list[str]:
-    """Returns a list of lines"""
-
-    return load_file(filename).split("\n")
-
-
-def load_ints(filename: str) -> list[int]:
-    """Returns a list of ints"""
-
-    return [int(i) for i in load_lines(filename)]
-
-if TEST:
-    ll = load_lines("05.test")
-else:
-    ll = load_lines("05.in")
-
-
-
-class Vents:
+class VentMap:
+    DIMENSION = 1000
     def __init__(self):
-        self.vents = [[0 for _ in range(1000)] for i in range(1000)]
+        self.vents = []
+        self.lines = []
 
-    # def line(self, x, y, dx, dy):
-    #     for i in range(x, dx+1):
-    #         for j in range(y, dy+1):
-    #             self.vents[i][j] += 1
-
-    def line(self, x1, y1, x2, y2):
-        # print(x1 == x2 or y1==y2)
-
-        if x1 > x2:
-            x1, x2 = x2, x1
-        if y1 > y2:
-            y1, y2 = y2, y1
-
-        if (x1 == x2 or y1 == y2):
-            for i in range(x1, x2+1):
-                for j in range(y1, y2+1):
-                    self.vents[j][i] += 1
-
-
-    def print(self):
-        for row in self.vents:
-            for num in row:
-                if num:
-                    print(num, end='')
-                else:
-                    print('.', end='')
-            print()
-
-
-vents = Vents()
-
-pattern = re.compile(r"(\d*),(\d*) -> (\d*),(\d*)")
-
-for line in ll:
-    a = pattern.match(line.strip())
-    x1 = int(a.group(1))
-    y1 = int(a.group(2))
-    x2 = int(a.group(3))
-    y2 = int(a.group(4))
-
-    # print(x1,y1,x2,y2)
-    vents.line(x1,y1,x2,y2)
-    # vents.print()
-    # input()
-
-
-total = 0
-for row in vents.vents:
-    for vent in row:
-        if vent > 1:
-            total += 1
-
-# print(total)
-
-
-# Part 2
-
-class Vents2:
-    def __init__(self):
-        self.vents = [[0 for _ in range(1000)] for i in range(1000)]
-
-    def line(self, x1, y1, x2, y2):
-        if (x1 == x2 or y1 == y2):
-            if x1 > x2:
-                x1, x2 = x2, x1
-            if y1 > y2:
-                y1, y2 = y2, y1
-            for i in range(x1, x2+1):
-                for j in range(y1, y2+1):
-                    self.vents[j][i] += 1
+    def make_line(self, line: Line, allow_diagonal):
+        line_length = abs(line.x1 - line.x2)
+        is_diagonal = True
+        if line.x1 < line.x2:
+            dx = 1
+        elif line.x1 > line.x2:
+            dx = -1
+        else:   # equal
+            dx = 0
+            line_length = abs(line.y1 - line.y2)
+            is_diagonal = False
         
-        else:  #diagonal
-            # a = (x1, y1)
-            # b = (x2, y2)
+        if line.y1 < line.y2:
+            dy = 1
+        elif line.y1 > line.y2:
+            dy = -1
+        else:   # equal
+            dy = 0
+            is_diagonal = False
 
-            # if a > b:
-            #     a, b = b, a
+        if not allow_diagonal and is_diagonal:
+            return
+        
+        for i in range(line_length + 1):
+            self.vents[line.y1 + (i * dy)][line.x1 + (i * dx)] += 1
 
-            # if a[0] < b[0]:
-            #     for i in range(b[0] - a[0] + 1):
-            #         print(i)
-            #         self.vents[a[1] + i][a[0] + i] += 1
-            # else:
-            #     for i in range(b[0] - a[0] + 1):
-            #         self.vents[a[1] - i][a[0] + i] += 1
+    def load(self, filename: str):
+        with open(filename) as f:
+            self.lines = [Line.from_string(line.strip()) for line in f.readlines()]
+    
+    def run(self, allow_diagonal=False):
 
-            if x1 < x2:
-                dx = 1
-            else:
-                dx = -1
+        self.vents = [[0 for _ in range(self.DIMENSION)] for i in range(self.DIMENSION)]
+        
+        for line in self.lines:
+            self.make_line(line, allow_diagonal=allow_diagonal)
 
-            if y1 < y2:
-                dy = 1
-            else:
-                dy = -1
-            
-            for i in range(abs(x1 - x2) + 1):
-                self.vents[y1 + (i * dy)][x1 + (i * dx)] += 1
+        return self.total
 
-
-
-    def print(self):
+    @property
+    def total(self):
+        total = 0
         for row in self.vents:
-            for num in row:
-                if num:
-                    print(num, end='')
-                else:
-                    print('.', end='')
-            # print()
+            for vent in row:
+                if vent > 1:
+                    total += 1
+        
+        return total
 
 
-vents = Vents2()
-
-pattern = re.compile(r"(\d*),(\d*) -> (\d*),(\d*)")
-
-for line in ll:
-    a = pattern.match(line.strip())
-    x1 = int(a.group(1))
-    y1 = int(a.group(2))
-    x2 = int(a.group(3))
-    y2 = int(a.group(4))
-
-    print(x1,y1,x2,y2)
-    vents.line(x1,y1,x2,y2)
-    # vents.print()
-    # input()
-
-
-total = 0
-for row in vents.vents:
-    for vent in row:
-        if vent > 1:
-            total += 1
-
-print(f"p2: {total}")
+vents = VentMap()
+vents.load("05.in")
+print(f"Part 1: {vents.run()}")
+print(f"Part 2: {vents.run(allow_diagonal=True)}")
