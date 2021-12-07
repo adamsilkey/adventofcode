f73160750a86b778220c711f12ddbb753dc3e12e
Adam Silkey <Adam.Silkey@gmail.com>
2021-12-06 23:25:10 -0600

    Cleanup 2021 - Day 07 - Fix Part 2
    Part 2 in my original code had an unfortuante error of data. The
    logical average (458) was not actually the right target to go for.
    Instead, it was 457 that gave me trouble. To fix this for any
    input, I wrote some code to check above and below the average,
    so that whatever it was, it would still give me the right answer.

--- 2021/day07.py
+++ 2021/day07.py
@@ -5,11 +5,7 @@ TEST = False
 
 AOC_DAY = '07'
 
-import itertools as it
-from collections import deque
-from dataclasses import dataclass
 from statistics import median
-
 
 def load_file(filename: str) -> str:
     """Loads an AOC file, returns a string"""
@@ -18,30 +14,10 @@ def load_file(filename: str) -> str:
         return f.read().rstrip("\n")
 
 
-def load_lines(filename: str) -> list[str]:
-    """Returns a list of lines"""
-
-    return load_file(filename).split("\n")
-
-
-def load_ints(filename: str) -> list[int]:
-    """Returns a list of ints"""
-
-    return [int(i) for i in load_lines(filename)]
-
-
 if TEST:
     filename = f"{AOC_DAY}.test"
 else:
     filename = f"{AOC_DAY}.in"
-
-
-
-print(median(ll))
-
-# median = 321
-
-from statistics import median
 
 ll = [int(i) for i in load_file(filename).split(',')]
 
@@ -49,20 +25,24 @@ total = 0
 for i in ll:
     total += abs(median(ll) - i)
 
-print(total)
+print(f"Part 1: {total}")
 
 average = sum(ll)/len(ll)
 average = round(average)
-print(average)
+# print(average)
+options = [average, average + 1, average - 1]
 
 # average = 458 #92439832
-average = 457
+# average = 457
 
-part2 = 0
-for i in ll:
-    difference = abs(average - i)
-    for i in range(difference + 1):
-        part2 += i
-
-print(part2)
-
+potentials = []
+for ave in options:
+    total = 0
+    for i in ll:
+        difference = abs(ave - i)
+        for i in range(difference + 1):
+            total += i
+    
+    potentials.append(total)
+    
+print(f"Part 2: {min(potentials)}")
