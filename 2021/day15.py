5dee05939ade5c8c0b1464e18d910fa2e8bece15
Adam Silkey <Adam.Silkey@gmail.com>
2021-12-16 22:07:46 -0600

    Final cleanup 2021 - Day 15

--- 2021/day15.py
+++ 2021/day15.py
@@ -3,6 +3,7 @@
 YEAR = '2021'
 AOC_DAY = '15'
 
+import heapq
 import itertools as it
 import math
 import sys
@@ -68,20 +69,49 @@ Node = namedtuple("Node", ["r", "c"])
 
 
 
-def load_puzzle(filename):
+def load_puzzle(filename, part):
     ll = load_lines(filename)
 
-    graph = []
-    for line in ll:
-        graph.append([int(c) for c in line])
+    if part == 1:
+        graph = []
+        for line in ll:
+            graph.append([int(c) for c in line])
 
-    max_r = len(graph)
-    max_c = len(graph[0])
+        max_r = len(graph)
+        max_c = len(graph[0])
 
-    distances = {}
-    for r in range(max_r):
-        for c in range(max_c):
-            distances[Node(r,c)] = math.inf
+        distances = {}
+        for r in range(max_r):
+            for c in range(max_c):
+                distances[Node(r,c)] = math.inf
+
+    elif part == 2:
+        graph = []
+        # increase = 0
+        for j in range(5):
+            for line in ll:
+                row = []
+                for i in range(5):
+                    offset = j + i
+                    for c in line:
+                        c = int(c) + offset
+                        if c > 9:
+                            c -= 9
+                        row.append(c)
+                graph.append(row)
+
+        max_r = len(graph)
+        max_c = len(graph[0])
+
+        distances = {}
+        for r in range(max_r):
+            for c in range(max_c):
+                distances[Node(r,c)] = math.inf
+
+    else:
+        print("Ya dun goofed this call, buddy boy: " + part)
+        import sys;sys.exit()
+
 
     distances[Node(0,0)] = 0
 
@@ -91,14 +121,14 @@ def load_puzzle(filename):
     def inbounds(node: Node):
         return 0 <= node.r < max_r and 0 <= node.c < max_c
 
-    check_next = {Node(0,0): 0}
+    # check_next = {Node(0,0): 0}
+    check_next = []
+    heapq.heappush(check_next, (0, Node(0,0)))
 
     return check_next, distances, not_visited, graph, inbounds
 
 
-
-
-def determine_shortest(check_next: dict, distances: dict, not_visited: set, graph, inbounds):
+def determine_shortest(check_next, distances: dict, not_visited: set, graph, inbounds):
 
     directions = [
         Node(-1, 0),    # N
@@ -107,12 +137,7 @@ def determine_shortest(check_next: dict, distances: dict, not_visited: set, grap
         Node(0, -1),    # W
     ]
 
-    shortest = min(check_next.values())
-    for node, distance in check_next.items():
-        if distances[node] == shortest:
-            break
-
-    check_next.pop(node)
+    node = heapq.heappop(check_next)[1]
 
     for d in directions:
         next_node = Node(node.r + d.r, node.c + d.c)
@@ -125,88 +150,45 @@ def determine_shortest(check_next: dict, distances: dict, not_visited: set, grap
         distance = graph[next_node.r][next_node.c] + distances[node]
         if distance < distances[next_node]:
             distances[next_node] = distance
-            check_next[next_node] = distance
+            heapq.heappush(check_next, (distance, next_node))
 
     not_visited.remove(node)
 
     return check_next, distances, not_visited
 
 
-# part 1
 
-def part_one():
-    check_next, distances, not_visited, graph, inbounds = load_puzzle(filename)
-    cn, ds, nv = check_next, distances, not_visited
-
-    while cn:
-        cn, ds, nv = determine_shortest(cn, ds, nv, graph, inbounds)
-        # print(cn)
-        # for k, v in ds.items():
-        #     if v != math.inf:
-        #         # print(k, v)
-        # print()
-        # input()
-    for k, v in ds.items():
-        print(k, v)
-
-# part_one()
-# import sys;sys.exit()
-
-
-def load_puzzle_part2(filename):
-    ll = load_lines(filename)
-
-    graph = []
-    # increase = 0
-    for j in range(5):
-        for line in ll:
-            row = []
-            for i in range(5):
-                offset = j + i
-                for c in line:
-                    c = int(c) + offset
-                    if c > 9:
-                        c -= 9
-                    row.append(c)
-            graph.append(row)
-
-    max_r = len(graph)
-    max_c = len(graph[0])
-
-    distances = {}
-    for r in range(max_r):
-        for c in range(max_c):
-            distances[Node(r,c)] = math.inf
-
-    distances[Node(0,0)] = 0
-
-    not_visited = set(distances.keys())
-    # not_visited.remove(Node(0,0))
-
-    def inbounds(node: Node):
-        return 0 <= node.r < max_r and 0 <= node.c < max_c
-
-    check_next = {Node(0,0): 0}
-
-    return check_next, distances, not_visited, graph, inbounds
-
-
-def part_two():
+def run():
     import time
+
+    # Part 1
     tic = time.perf_counter()
-    check_next, distances, not_visited, graph, inbounds = load_puzzle_part2(filename)
+    check_next, distances, not_visited, graph, inbounds = load_puzzle(filename, 1)
     cn, ds, nv = check_next, distances, not_visited
 
     while cn:
         cn, ds, nv = determine_shortest(cn, ds, nv, graph, inbounds)
 
     last_node = Node(len(graph) - 1, len(graph[0]) - 1)
-    print(last_node, ds[last_node])
+    print(f"Part 1: {last_node} {ds[last_node]}")
     toc = time.perf_counter()
     print(f"Elapsed: {toc - tic:0.4f} seconds")
 
 
-part_two()
+    # Part 2
+    tic = time.perf_counter()
+    check_next, distances, not_visited, graph, inbounds = load_puzzle(filename, 2)
+    cn, ds, nv = check_next, distances, not_visited
+
+    while cn:
+        cn, ds, nv = determine_shortest(cn, ds, nv, graph, inbounds)
+
+    last_node = Node(len(graph) - 1, len(graph[0]) - 1)
+    print(f"Part 2: {last_node} {ds[last_node]}")
+    toc = time.perf_counter()
+    print(f"Elapsed: {toc - tic:0.4f} seconds")
+
+run()
 
 
 
