95b58f0e9a01ebd4efd9c387aedbd5bed3841107
Adam Silkey <Adam.Silkey@gmail.com>
2022-11-30 23:43:24 -0600

    Solve 2022 - Day 01

---
+++ 2022/day01.py
@@ -0,0 +1,127 @@
+#! /usr/bin/env python3.11
+
+YEAR = '2022'
+AOC_DAY = '01'
+
+import itertools as it
+import sys
+from collections import Counter, defaultdict, deque, namedtuple
+from dataclasses import dataclass
+
+if not (YEAR and AOC_DAY):
+    print("!!! Set YEAR/AOC_DAY")
+    sys.exit(1)
+
+def fail():
+    print("!!! Need to define -p for production or -t for test")
+    sys.exit(1)
+
+if len(sys.argv) != 2:
+    fail()
+
+match sys.argv[1].lower():
+    case '-t':
+        test = True
+    case '-p':
+        test = False
+    case _:
+        fail()
+
+if test:
+    filename = f"input/{AOC_DAY}.test"
+else:
+    filename = f"input/{AOC_DAY}.in"
+
+title = f"Advent of Code {YEAR} - Day {AOC_DAY} - {'Test' if test else 'Production'}"
+
+print(f"=" * len(title))
+print(title)
+print(f"=" * len(title))
+
+def load_file(filename: str) -> str:
+    """Loads an AOC file, returns a string"""
+
+    with open(filename) as f:
+        return f.read().rstrip("\n")
+
+
+def load_lines(filename: str) -> list[str]:
+    """Returns a list of lines"""
+
+    return load_file(filename).split("\n")
+
+
+def load_ints(filename: str) -> list[int]:
+    """Returns a list of ints"""
+
+    return [int(i) for i in load_lines(filename)]
+
+
+def load_comma_separated_ints(filename: str) -> list[int]:
+    """Returns a list of ints from a comma separated list of ints"""
+
+    return [int(i) for i in load_file(filename).strip().split(",")]
+
+
+
+print(filename)
+
+
+inp = load_lines(filename)
+print(inp)
+
+largest = 0
+current = 0
+
+lis = []
+
+for x in inp:
+    if x == '':
+        lis.append(current)
+        if current > largest:
+            largest = current
+        current = 0
+    else:
+        current += int(x)
+        
+print(largest)
+
+lis.sort()
+
+print(lis)
+
+
+
+print(sum([69172, 71575, 73211]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if test:
+    print()
+    print("============ This was a test!!!! ============")
+else:
+    print()
+    print("You just ran that production data. Nice work!")
