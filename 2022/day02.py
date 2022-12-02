a5275da2a0cc065dd5eee9192c50d19343cdc9ea
Adam Silkey <Adam.Silkey@gmail.com>
2022-12-01 23:16:26 -0600

    Solve 2022 - Day 02

---
+++ 2022/day02.py
@@ -0,0 +1,145 @@
+#! /usr/bin/env python3.11
+
+YEAR = '2022'
+AOC_DAY = '02'
+
+import itertools as it
+import sys
+from collections import Counter, defaultdict, deque, namedtuple
+from dataclasses import dataclass
+
+if not (YEAR and AOC_DAY):
+    print("!!! Set YEAR/AOC_DAY")
+    sys.exit(1)
+
+def fail():
+    print("!!! Need to define -p for production or -t for test")
+    sys.exit(1)
+
+if len(sys.argv) != 2:
+    fail()
+
+match sys.argv[1].lower():
+    case '-t':
+        test = True
+    case '-p':
+        test = False
+    case _:
+        fail()
+
+if test:
+    FILENAME = f"input/{AOC_DAY}.test"
+else:
+    FILENAME = f"input/{AOC_DAY}.in"
+
+title = f"Advent of Code {YEAR} - Day {AOC_DAY} - {'Test' if test else 'Production'}"
+
+print(f"=" * len(title))
+print(title)
+print(f"=" * len(title))
+
+def load_file(filename: str = FILENAME) -> str:
+    """Loads an AOC file, returns a string"""
+
+    with open(filename) as f:
+        return f.read().rstrip("\n")
+
+
+def load_lines(filename: str = FILENAME) -> list[str]:
+    """Returns a list of lines"""
+
+    return load_file(filename).split("\n")
+
+
+def load_ints(filename: str = FILENAME) -> list[int]:
+    """Returns a list of ints"""
+
+    return [int(i) for i in load_lines(filename)]
+
+
+def load_comma_separated_ints(filename: str = FILENAME) -> list[int]:
+    """Returns a list of ints from a comma separated list of ints"""
+
+    return [int(i) for i in load_file(filename).strip().split(",")]
+
+
+
+p = load_lines()
+
+strategy = [()]
+
+# A rock x rock
+# b pap y pap
+# c sci z sci
+strategy = {
+    "A X": 3, # 
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9, 
+    "C X": 2, 
+    "C Y": 6,
+    "C Z": 7,
+}
+
+r1_strategy = {
+    "A X": 4, # 
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9, # win
+    "C X": 7, # win
+    "C Y": 2,
+    "C Z": 6,
+}
+
+score = 0
+for line in p:
+    score += strategy[line]
+
+print(score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if test:
+    print()
+    print("============ This was a test!!!! ============")
+else:
+    print()
+    print("You just ran that production data. Nice work!")
