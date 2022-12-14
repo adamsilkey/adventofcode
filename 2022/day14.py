6f01c8eef9c8fcf3c86919c4bbf11de25390ae75
Adam Silkey <Adam.Silkey@gmail.com>
2022-12-14 00:31:47 -0600

    Solve 2022 Day 14 Part 2

--- 2022/day14.py
+++ 2022/day14.py
@@ -102,7 +102,143 @@ DIRS = dict(
     DR = Node(1, 1),
 )
 
-class Cave:
+class CavePart2:
+    SAND_ORIGIN = Node(0, 500)
+
+    def __init__(self, layout, left_col, right_col, lower_row):
+        self.layout = layout
+        self.left_col = left_col
+        self.right_col = right_col
+        self.lower_row = lower_row
+        self.total_sand = 0
+
+        self.grid = []
+        for r in range(0, lower_row + 2): # one more row for part 2
+            row = ['.' for c in range(1000)]
+            self.grid.append(row)
+
+        # add the infinite floor
+        row = ['#' for c in range(1000)]
+        self.grid.append(row)
+
+        for rockform in layout:
+            self.add_rock_formation(rockform)
+    
+    def inbounds(self, node: Node):
+        return 0 <= node.r < self.dimension and self.left_col <= node.c < self.right_col
+
+    @classmethod
+    def fromfile(cls, filename):
+        layout = []
+        left_col = math.inf
+        right_col = -math.inf
+        lower_row = 0
+        with open(filename) as f:
+            for line in f:
+                rockstructure = []
+                coords = line.split(' -> ')
+                # print(coords)
+                for coord in coords:
+                    c, r = coord.split(',')
+                    c = int(c)
+                    r = int(r)
+                    rockstructure.append(Node(r, c))
+
+                    # find the left/right bounds
+                    if c < left_col:
+                        left_col = c
+                    if c > right_col:
+                        right_col = c
+                    if r > lower_row:
+                        lower_row = r
+
+
+                layout.append(rockstructure)
+        
+        return cls(layout, left_col, right_col, lower_row)
+
+    def add_rock_formation(self, rockformation):
+        for start, end in zip(rockformation[0:-1], rockformation[1:]):
+
+            start = Node(start.r, start.c)
+            end = Node(end.r, end.c)
+
+            if start > end:
+                end, start = start, end
+            
+            if start.c - end.c == 0:
+                for r in range(start.r, end.r+1):
+                    self.grid[r][start.c] = '#'
+            else: # column
+                for c in range(start.c, end.c+1):
+                    self.grid[start.r][c] = '#'
+
+    # def __repr__(self):
+    #     return f"Cave(cavestring={self.cavestring})"
+
+    def __str__(self):
+        final_string = ''
+        for row in self.grid:
+            final_string += ''.join(row)
+            final_string += '\n'
+        
+        return final_string
+    
+    def drop_sand(self):
+        sand = Node(self.SAND_ORIGIN.r, self.SAND_ORIGIN.c)
+
+        # check next
+        while True:
+            blocks = ['#', 'o']
+            d = DIRS["D"]
+            dr = DIRS["DR"]
+            dl = DIRS["DL"]
+
+            next = Node(sand.r + d.r, sand.c + d.c)
+            if not self.grid[next.r][next.c] in blocks:
+                sand = Node(next.r, next.c)
+                continue
+            next = Node(sand.r + dl.r, sand.c + dl.c)
+            if not self.grid[next.r][next.c] in blocks:
+                sand = Node(next.r, next.c)
+                continue
+            next = Node(sand.r + dr.r, sand.c + dr.c)
+            if not self.grid[next.r][next.c] in blocks:
+                sand = Node(next.r, next.c)
+                continue
+
+            self.grid[sand.r][sand.c] = 'o'
+            self.total_sand += 1
+            if sand == self.SAND_ORIGIN:
+                print(self.total_sand)
+                return True
+            break
+
+    def run(self):
+        while True:
+            if self.drop_sand():
+                break
+
+
+
+
+cave = CavePart2.fromfile(FILENAME)
+# print(cave)
+cave.run()
+# print(cave)
+
+
+
+
+
+
+
+
+
+
+sys.exit()
+
+class CavePart1:
     SAND_ORIGIN = Node(0, 500)
 
     def __init__(self, layout, left_col, right_col, lower_row):
@@ -216,11 +352,10 @@ class Cave:
 
 
 
-    # def build_cave(self, cavestring, corner_r, corner_c):
-
-cave = Cave.fromfile(FILENAME)
-print(cave)
+cave = CavePart1.fromfile(FILENAME)
+# print(cave)
 cave.run()
+print(cave)
 
 
 
