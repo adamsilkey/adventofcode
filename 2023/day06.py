494cdc56eec092d1f98e91cbde18fe562b247ea6
Adam Silkey <Adam.Silkey@gmail.com>
2023-12-05 23:29:49 -0600

    Solve 2023 Day 06

---
+++ 2023/day06.py
@@ -0,0 +1,135 @@
+#! /usr/bin/env python3
+
+YEAR = '2023'
+AOC_DAY = '06'
+
+import sys
+
+if not (YEAR and AOC_DAY):
+    print("!!! Set YEAR/AOC_DAY")
+    sys.exit(1)
+
+def fail():
+    print("!!! Need to define -p for production or -t for test")
+    sys.exit(1)
+
+if len(sys.argv) != 2:
+    fail()
+
+match sys.argv[1].lower():
+    case '-t':
+        test = True
+    case '-p':
+        test = False
+    case _:
+        fail()
+
+if test:
+    filename = f"input/{AOC_DAY}.test"
+else:
+    filename = f"input/{AOC_DAY}.in"
+
+title = f"Advent of Code {YEAR} - Day {AOC_DAY} - {'Test' if test else 'Production'}"
+
+print(f"=" * len(title))
+print(title)
+print(f"=" * len(title))
+
+def load_file(filename: str) -> str:
+    """Loads an AOC file, returns a string"""
+
+    with open(filename) as f:
+        return f.read().rstrip("\n")
+
+
+def load_lines(filename: str) -> list[str]:
+    """Returns a list of lines"""
+
+    return load_file(filename).split("\n")
+
+
+def load_ints(filename: str) -> list[int]:
+    """Returns a list of ints"""
+
+    return [int(i) for i in load_lines(filename)]
+
+
+def load_comma_separated_ints(filename: str) -> list[int]:
+    """Returns a list of ints from a comma separated list of ints"""
+
+    return [int(i) for i in load_file(filename).strip().split(",")]
+
+
+import functools
+import itertools as it
+import re
+import sys
+from collections import Counter, defaultdict, deque, namedtuple
+from dataclasses import dataclass
+
+
+inp = load_lines(filename)
+
+# Race = namedtuple("Race", "time distance")
+
+# test = [
+    # "imagine the test inputs"
+# ]
+
+# prod = [
+    # "imagine the prod inputs"
+# ]
+
+# def calc_winners(race: Race):
+#     winners = []
+#     for speed in range(race.time+1):
+#         time = race.time - speed
+#         if time * speed > race.distance:
+#             winners.append(speed)
+    
+#     return len(winners)
+
+# for race in prod:
+#     print(calc_winners(race))
+
+
+time = "your input time here"
+distance = "your distance time here"
+
+# test = time // 2
+
+# print(test * test) 
+# print(test * test > distance)
+
+losers = 0
+for speed in range(time+1):
+    t = time - speed
+    if t * speed <= distance:
+        losers += 1
+
+    else:
+        break
+
+print(time - losers - losers + 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if test:
+    print()
+    print("============ This was a test!!!! ============")
+else:
+    print()
+    print("You just ran that production data. Nice work!")
