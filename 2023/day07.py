066725a9d60b45145b36298c425eb2a955873a2d
Adam Silkey <Adam.Silkey@gmail.com>
2023-12-07 00:16:17 -0600

    Cleanup 2023 Day 07

--- 2023/day07.py
+++ 2023/day07.py
@@ -71,36 +71,40 @@ from math import prod
 
 inp = load_lines(filename)
 
+
 class Hand:
     
-    CARD_VALUE = {
-        "A"
-    }
-    def __init__(self, hand: str, bid: int):
+    def __init__(self, hand: str, bid: int, version=1):
         self.hand = hand
         self.bid = bid
+        self.version = version
     
     def __repr__(self):
-        return self.hand
+        return f"Hand(hand={self.hand!r},bid={self.bid!r}, version={self.version!r})"
     
     def __str__(self):
         return self.hand
     
     def __eq__(self, other):
-        if not self.rank == other.rank:
+        if self.rank != other.rank:
             return False
         else:
+            # Check every value in turn to see if they're not equal
             for left, right in zip(self.hand, other.hand):
                 left = self.convert_card(left)
                 right = self.convert_card(right)
 
                 if left != right:
                     return False
-            else: # no break
+            else: # no break, they're equal
                 return True
 
     def __gt__(self, other):
-        if self.rank == other.rank:
+        # check if hand rank doesn't match
+        if self.rank != other.rank:
+            return self.rank > other.rank
+        # check values in order, skipping over equal cards
+        else:
             for left, right in zip(self.hand, other.hand):
                 left = self.convert_card(left)
                 right = self.convert_card(right)
@@ -108,12 +112,14 @@ class Hand:
                 if left != right:
                     return left > right
             else: # equal hands
-                return left > right
-        else:
-            return self.rank > other.rank
+                return False
 
     def __lt__(self, other):
-        if self.rank == other.rank:
+        # check if hand rank doesn't match
+        if self.rank != other.rank:
+            return self.rank < other.rank
+        # check values in order, skipping over equal cards
+        else:
             for left, right in zip(self.hand, other.hand):
                 left = self.convert_card(left)
                 right = self.convert_card(right)
@@ -121,9 +127,7 @@ class Hand:
                 if left != right:
                     return left < right
             else: # equal hands
-                return left < right
-        else:
-            return self.rank < other.rank
+                return False
 
     def __ge__(self, other):
         return self.__gt__(other) or self.__eq__(other)
@@ -131,136 +135,41 @@ class Hand:
     def __le__(self, other):
         return self.__lt__(other) or self.__eq__(other)
 
-    @staticmethod
-    def convert_card(s: str) -> int:
-        s = s.replace("A", "14").replace("T", "10").replace("J", "11").replace("Q", "12").replace("K", "13")
+    def convert_card(self, char_: str, /) -> int:
+        """Converts card string to card rank value"""
 
-        return int(s)
+        if len(char_) != 1:
+            raise ValueError(f"Invalid character passed to convert_card(): {char_}")
 
+        # Jack value is 11 / Joker value is 0
+        j_value = "11" if self.version == 1 else "0"
+        char_ = char_.replace("A", "14").replace("T", "10").replace("J", j_value).replace("Q", "12").replace("K", "13")
 
-    @staticmethod
-    def compare_hand(left: list[int], right: list[int]):
-        lc = Counter(left)
-        rc = Counter(right)
-        pass
-
-    @property
-    def rank(self):
-        hc = Counter(self.hand)
-
-        match len(hc):
-            case 1: # five of a kind
-                return 7
-            case 2: # four of a kind or full house
-                if 4 in hc.values():
-                    return 6
-                else:
-                    return 5
-            case 3: # three of a kind or two pair
-                if 3 in hc.values():
-                    return 4
-                else:
-                    return 3
-            case 4: # one pair
-                return 2
-            case 5:
-                return 1
-
-
-class Hand2:
-    
-    def __init__(self, hand: str, bid: int):
-        self.hand = hand
-        self.bid = bid
-    
-    def __repr__(self):
-        return self.hand
-    
-    def __str__(self):
-        return self.hand
-    
-    def __eq__(self, other):
-        if not self.rank == other.rank:
-            return False
-        else:
-            for left, right in zip(self.hand, other.hand):
-                left = self.convert_card(left)
-                right = self.convert_card(right)
-
-                if left != right:
-                    return False
-            else: # no break
-                return True
-
-    def __gt__(self, other):
-        if self.rank == other.rank:
-            for left, right in zip(self.hand, other.hand):
-                left = self.convert_card(left)
-                right = self.convert_card(right)
-
-                if left != right:
-                    return left > right
-            else: # equal hands
-                return left > right
-        else:
-            return self.rank > other.rank
-
-    def __lt__(self, other):
-        if self.rank == other.rank:
-            for left, right in zip(self.hand, other.hand):
-                left = self.convert_card(left)
-                right = self.convert_card(right)
-
-                if left != right:
-                    return left < right
-            else: # equal hands
-                return left < right
-        else:
-            return self.rank < other.rank
-
-    def __ge__(self, other):
-        return self.__gt__(other) or self.__eq__(other)
-
-    def __le__(self, other):
-        return self.__lt__(other) or self.__eq__(other)
-
-    @staticmethod
-    def convert_card(s: str) -> int:
-        s = s.replace("A", "14").replace("T", "10").replace("J", "1").replace("Q", "12").replace("K", "13")
-
-        return int(s)
-
-
-    @staticmethod
-    def compare_hand(left: list[int], right: list[int]):
-        lc = Counter(left)
-        rc = Counter(right)
-        pass
+        return int(char_)
 
     @property
     def rank(self):
         hc = Counter(self.hand)
 
-        if 'J' in hc:
+        # Part 2
+        if self.version == 2 and 'J' in hc:
             if len(hc) == 1:
                 return 7 # five of a kind
-            # print(hand, 'found J')
             joker_hc = Counter(self.hand)
             num_js = joker_hc['J']
-            # print(num_js)
             del joker_hc['J']
-            # print(joker_hc)
-            try:
-                key = joker_hc.most_common(1)[0][0]
-            except IndexError:
-                print(self.hand)
-                sys.exit()
+            key = joker_hc.most_common(1)[0][0]
             joker_hc[key] += num_js
 
-        
             hc = joker_hc
-            # print('joker', hc)
 
+        # Because we have five card hands, we know that the counter values
+        # of certain hands will equate to certain hand types
+        # E.g. 'AAAA4' will convert to {'A': 4, '4': 1}.
+        # With a length of 2, we know it's four of a kind
+        #
+        # You could have just as easily done this by looking at values instead
+        # but length is what I came up with in the moment
         match len(hc):
             case 1: # five of a kind
                 return 7
@@ -280,29 +189,28 @@ class Hand2:
                 return 1
 
 
-
-
-
-
-hands = []
+p1 = []
+p2 = []
 for line in inp:
-    a, b = line.split()
+    hand, bid = line.split()
     
-    hands.append(Hand2(a, int(b)))
+    p1.append(Hand(hand, int(bid)))
+    p2.append(Hand(hand, int(bid), version=2))
 
-for hand in hands:
-    print(hand.hand, hand.rank)
+p1.sort()
+p2.sort()
 
-hands.sort()
+p1_result = 0
+p2_result = 0
 
+for i, hand in enumerate(p1, 1):
+    p1_result += hand.bid * i
 
-p1 = 0
+for i, hand in enumerate(p2, 1):
+    p2_result += hand.bid * i
 
-for i, hand in enumerate(hands, 1):
-    p1 += hand.bid * i
-
-print(p1)
-# print(hands)
+print(f"{p1_result=}")
+print(f"{p2_result=}")
 
 
 
