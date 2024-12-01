ac8a9e7f3385442bd0e9960ffdcb94f67d9a5283
Adam Silkey <Adam.Silkey@gmail.com>
2024-11-30 23:19:50 -0600

    Solve 2024 Day 1 - Rank 628/813

---
+++ 2024/day01.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python3
+
+YEAR = "2024"
+AOC_DAY = "01"
+
+import sys
+from time import perf_counter
+
+if not (YEAR and AOC_DAY):
+    print("!!! Set YEAR/AOC_DAY")
+    sys.exit(1)
+
+
+def fail():
+    print("!!! Need to define -p for production or -t for test")
+    sys.exit(1)
+
+
+if len(sys.argv) != 2:
+    fail()
+
+match sys.argv[1].lower():
+    case "-t":
+        test = True
+    case "-p":
+        test = False
+    case _:
+        fail()
+
+if test:
+    filename = f"input/{AOC_DAY}.test"
+else:
+    filename = f"input/{AOC_DAY}.in"
+
+title = f"Advent of Code {YEAR} - Day {AOC_DAY} - {'Test' if test else 'Production'}"
+
+print(f"=" * len(title))
+print(title)
+print(f"=" * len(title))
+
+
+def load_file(filename: str) -> str:
+    """Loads an AOC file, returns a string"""
+
+    with open(filename) as f:
+        return f.read().rstrip("\n")
+
+
+def load_lines(filename: str) -> list[str]:
+    """Returns a list of lines"""
+
+    return load_file(filename).split("\n")
+
+
+def load_ints(filename: str) -> list[int]:
+    """Returns a list of ints"""
+
+    return [int(i) for i in load_lines(filename)]
+
+
+def load_comma_separated_ints(filename: str) -> list[int]:
+    """Returns a list of ints from a comma separated list of ints"""
+
+    return [int(i) for i in load_file(filename).strip().split(",")]
+
+
+import functools
+import itertools as it
+import re
+import sys
+from collections import Counter, defaultdict, deque, namedtuple
+from dataclasses import dataclass
+from math import lcm, prod
+
+DEBUG = True
+
+__start_time = perf_counter()
+
+
+# inp = load_file(filename).split(',')
+inp = load_lines(filename)
+# inp = load_ints(filename)
+
+left = []
+right = []
+
+for line in inp:
+    a, b = [int(a) for a in line.split()]
+    left.append(a)
+    right.append(b)
+
+left.sort()
+right.sort()
+
+counter = Counter()
+
+for i in right:
+    counter[i] += 1
+
+print(counter)
+
+# print(left)
+
+p1 = 0
+
+for i, a in enumerate(left):
+    p1 += abs(a - right[i])
+
+print(p1)
+
+print("p2")
+p2 = 0
+for i in left:
+    if i in counter:
+        p2 += i * counter[i]
+
+print(p2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if test:
+    print()
+    print("============ This was a test!!!! ============")
+else:
+    print()
+    print("You just ran that production data. Nice work!")
+print("Execution time:", perf_counter() - __start_time)
+print()
