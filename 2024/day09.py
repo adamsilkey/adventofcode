177fbf35e352a1983c95ca01136f0de2b5c5f121
Adam Silkey <Adam.Silkey@gmail.com>
2024-12-09 01:09:38 -0600

    Solve 2024 Day 09 Part 2
    Wow, that was a messy Part 2. Gotta really dig ito the crumminess of
    that code and figure out how to do it better. But! Still was able to
    solve it in the end.

--- 2024/day09.py
+++ 2024/day09.py
@@ -139,8 +139,168 @@ class Map:
 
 inp = load_file(filename)
 
-diskmap = []
 
+
+diskmap = []
+num = True
+disk_id = 0
+current = None
+current_len = 0
+
+for c in inp:
+    c = int(c)
+    if num:
+        for i in range(c):
+            diskmap.append(disk_id)
+
+        disk_id += 1
+    else:
+        for i in range(c):
+            diskmap.append(None)
+    num = not num
+
+
+def diskmapstr():
+    s = []
+    for c in diskmap:
+        if c is None:
+            c == '.'
+        s.append(str(c))
+
+    return ''.join(s)
+
+def diskmapprint():
+    for c in diskmap:
+        print(c, end='') if c is not None else print('.', end='')
+    print()
+
+
+free_ranges = []
+current = diskmap[0]
+num = True
+start = end = 0
+for idx, c in enumerate(diskmap):
+    if c != current:
+        end = idx
+        if current is None:
+            length = end - start
+            free_ranges.append((length, start, end))
+
+            # print(f"{current=}")
+            # print(length)
+        current = c
+    start = end
+
+# for free in free_ranges:
+#     print(free)
+
+# diskmapprint()
+
+start = len(diskmap)
+end = len(diskmap)
+current = diskmap[-1]
+
+target = diskmapstr()
+while True:
+    if start <= 0:
+        break
+    # iterate backwards, getting the size of the current range
+    while True:
+        start -= 1
+        if current != diskmap[start]:
+            break
+    # move start back one
+    start += 1
+    l = end - start
+
+    if current:
+        # print(f"{current=}")
+        found = None
+
+        # find the first hole that will fit
+        for idx, freespace in enumerate(free_ranges):
+            if l <= freespace[0] and freespace[1] < start:
+                found = free_ranges.pop(idx)
+                # print(f"{found=}")
+                # we have found a space that will fit
+                break
+
+        if found is not None:
+            # print(f"found our backwards range: {start, end}")
+            # insert new crap there
+            free_l, free_s, free_e = freespace
+            for i in range(l):
+                diskmap[free_s + i] = current
+                diskmap[start + i] = None
+            # diskmapprint()
+
+            # find the remaining free space and reinsert
+            if free_l > l:
+                new_free_range = (free_l - l, free_s + l, free_e)
+                # print(f"{new_free_range=}")
+                # print(free_ranges)
+                # input()
+
+                # merge any before and afters
+                # merge any befores
+                while True:
+                    fl, fs, fe = new_free_range
+                    if idx == 0:
+                        break
+
+                    bl, bs, be = free_ranges[idx - 1]
+                    if be == new_free_range[1]:
+                        new_free_range = (fe - bs, bs, fe)
+                        free_ranges.pop(idx - 1)
+                        idx -= 1
+                    else:
+                        break
+
+                # merge any afters
+                while True:
+                    fl, fs, fe = new_free_range
+                    if idx == len(free_ranges) - 1:
+                        break
+
+                    afl, afs, afe = free_ranges[idx + 1]
+                    if new_free_range[2] == afs:
+                        new_free_range = (afe - fs, fs, afe)
+                        free_ranges.pop(idx + 1)
+                    else:
+                        break
+
+                free_ranges.insert(idx, new_free_range)
+            # print(diskmap[start:end])
+
+    current = diskmap[start - 1]
+    end = start
+
+
+# diskmapprint()
+print('calcluating p2')
+for i, n in enumerate(diskmap):
+    if n is not None:
+        p2 += i * int(n)
+
+# import sys;sys.exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# part 1=================
+
+diskmap = []
 num = True
 disk_id = 0
 current = None
@@ -189,7 +349,7 @@ while True:
 
 
 
-diskmapprint()
+# diskmapprint()
 
 for i, n in enumerate(diskmap):
     p1 += i * int(n)
